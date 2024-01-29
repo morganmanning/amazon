2533d4d396889fcc0679142c024af1ca5cdfb0ce
morganmanning <morgan5761@gmail.com>
2024-01-29 09:06:44 -0500

    daily push

--- Global/occupancyEstimatesPlots.R
+++ Global/occupancyEstimatesPlots.R
@@ -184,15 +184,15 @@ for (i in 1:length(communities)) {
   
   # output lists
   clumpPlotMasterList[[i]] <- clumpPlotList
-  names(clumpPlotMasterList) <- communities
   unclumpPlotMasterList[[i]] <- unclumpPlotList
-  names(unclumpPlotMasterList) <- communities
   ufoMasterList[[i]] <- ufoList
-  names(ufoMasterList) <- communities
   unclumpedUFOMasterList[[i]] <- unclumpedUFOList 
-  names(unclumpedUFOMasterList) <- communities
   
 }
+names(clumpPlotMasterList) <- communities
+names(unclumpPlotMasterList) <- communities
+names(ufoMasterList) <- communities
+names(unclumpedUFOMasterList) <- communities
 
 
 
@@ -212,6 +212,11 @@ for (i in 1:length(communities)) {
   # detection: c("1")
   # occupancy: c("1)
 
+# output lists:
+masterBestofTheBest <- list() # occu output for the best model for each species
+masterTopModels <- list() # df of the models within 2 AIC of the top model
+masterBestModsFitLists <- list() # fit list of the best models for model averaging
+
 for (i in 1:length(communities)) {
   
   # set the variables to be considered for America's Next Top Model
@@ -226,7 +231,8 @@ for (i in 1:length(communities)) {
     match_occupancy <- c("1")
   }
   
-  ##### BEST DETECTION MODEL PER SPECIES
+  
+  ############### BEST DETECTION MODEL PER SPECIES
   # every possible combination of variables
   combos <- sapply(seq(length(match_detection)), function(k) {
     as.list(as.data.frame(combn(x = match_detection, m = k)))
@@ -246,42 +252,224 @@ for (i in 1:length(communities)) {
   
   # see which detection model is the best with null occupancy
   for (j in 1:length(speciesNames)){
+    # make a df off all detection formulas and their AIC for each species
+    detectionMods <- list()
+    temp <- data.frame(Model = allDetectionFormulas,
+                       AIC = NA)
     
-    ############ STOPPED HERE ###############
-    for (k in 1:length(ufoMasterList[[i]])) { # for every community???
-      detectionMods <- list()
-      temp <- data.frame(Model = allDetectionFormulas,
-                         AIC = NA)
-      for(m in 1:length(allDetectionFormulas)) {
-        test <- occu(formula(allDetectionFormulas[[m]]), ufoList[[k]])
-        detectionMods[[m]] <- occu(formula(allDetectionFormulas[[m]]), ufoList[[k]], 
-                                   control = 10000, 
-                                   starts = c(rep(0, length(test@opt$par)))) 
-        temp$AIC[m] <- detectionMods[[m]]@AIC
-      }
-      # select the model with the lowest AIC
-      bestDetectionModels$bestDetectionModel[k] <- detectionFormulas[which(temp$AIC == min(temp$AIC))]
+    # run occu model for every detection formula per species per community to compare AIC
+    for(m in 1:length(allDetectionFormulas)) {
+      test <- occu(formula(allDetectionFormulas[[m]]), ufoMasterList[[i]][[j]])
+      detectionMods[[m]] <- occu(formula(allDetectionFormulas[[m]]), ufoMasterList[[i]][[j]], 
+                                 control = 10000, 
+                                 starts = c(rep(0, length(test@opt$par)))) 
+      temp$AIC[m] <- detectionMods[[m]]@AIC
     }
+    # select the model with the lowest AIC
+    bestDetectionModels$bestDetectionModel[j] <- detectionFormulas[which(temp$AIC == min(temp$AIC))]
     
-    
+    # output is dataframe for community i with the best detection formulas for each species
+      
   }
   
   
   
+  ############# BEST OCCUPANCY MODEL PER SPECIES USING THE BEST DETECTION MODEL
+  # every possible combination of variables
+  combos <- sapply( seq(length(match_occupancy)), function(i) {
+    as.list(as.data.frame(combn( x = match_occupancy, m = i)))
+  })
+  combos <- unlist(combos, recursive=FALSE)
   
-  ##### BEST OCCUPANCY MODEL
+  # all combinations of variables into formulas
+  forms <- sapply(combos, function(x) paste("~ ", paste(x, collapse="+"), sep = ""))
+  
+  forms <- as.vector(c(forms, "~ 1")) # add the null model
+  
+  occupancyFormulas <- forms
   
   
   
+  ############### COMBINE BEST DETECTION WITH ALL POSSIBLE OCCUPANCY PREDICTORS AND MODEL
+  occupancyModelsList <- list()
+  for (j in 1:length(speciesNames)) {
+    df <- data.frame(detection = bestDetectionModels$bestDetectionModel[j],
+                     occupancy = occupancyFormulas)
+    occupancyModelsList[[j]] <- c(paste(df$detection, df$occupancy, sep = " "))
+  }
   
+  # run occupancy unmarked model for all models
+  allModels <- list()
+  for (j in 1:length(speciesNames)) { # for all the species
+    occupancyMods <- list()
+    for(m in 1:length(occupancyModelsList[[j]])) {
+      test <- occu(formula(occupancyModelsList[[j]][m]), ufoMasterList[[i]][[j]])
+      occupancyMods[[m]] <- occu(formula(occupancyModelsList[[j]][m]), ufoMasterList[[i]][[j]], 
+                                 control = 10000, 
+                                 starts = c(rep(0, length(test@opt$par)))) 
+    }
+    names(occupancyMods) <- 1:length(occupancyMods)
+    allModels[[j]] <- occupancyMods
+  }
+  
+  # Make a data frame to show the model names and their AICs
+  modelAICs <- list() # all models and their AICs
+  topModels <- list() # only models within 2 AIC of lowest AIC
+  for(j in 1:length(allModels)) { # for each species
+    df <- data.frame(ModelName = NA,
+                     AIC = NA,
+                     diffFromBest = NA)
+    for(m in 1:length(allModels[[j]])){
+      df[m,1]<- as.character(c(allModels[[j]][[m]]@formula))
+      df[m,2]<- allModels[[j]][[m]]@AIC
+    }
+    df <- df[order(df$AIC),]
+    df$diffFromBest <- df$AIC - min(df$AIC)
+    modelAICs[[j]] <- df
+    
+    # only the best
+    ANTM <- subset(df, diffFromBest <= 2)
+    topModels[[j]] <- ANTM
+  }
+  names(topModels) <- speciesNames
+  
+  masterTopModels[[i]] <- topModels
+  
+  # take these best models and put them into a list
+  bestModsFitLists <- list()
+  for (j in 1:length(topModels)){ # for each species
+    speciesBestMods <- list()
+    for(m in 1:nrow(topModels[[j]])) {
+      test <- occu(formula(topModels[[j]]$ModelName[m]), ufoMasterList[[i]][[j]])
+      
+      speciesBestMods[[m]] <- occu(formula(topModels[[j]]$ModelName[m]), ufoMasterList[[i]][[j]], 
+                                   control = 10000, 
+                                   starts = c(rep(0, length(test@opt$par)))) 
+    } 
+    bestModsFitLists[[j]] <- fitList(speciesBestMods)
+  }
+  names(bestModsFitLists) <- speciesNames
+  
+  masterBestModsFitLists[[i]] <- bestModsFitLists
+  
+  # just the #1 model for each species
+  bestOfTheBest <- list()
+  for (j in 1:length(speciesNames)) {
+    bestOfTheBest[[j]] <- occu(formula(topModels[[j]]$ModelName[1]), ufoMasterList[[i]][[j]])
+  }
+  names(bestOfTheBest) <- speciesNames
+  
+  masterBestofTheBest[[i]] <- bestOfTheBest
   
 }
 
 
   
+################################################################################
+########################## ESTIMATE CALCULATING ################################
+################################################################################
 
+# output lists:
+masterEstimatedParameters <- list() # df of occ/det and their SE/range
+masterUnmarkedPredOcc <- list() # unmarked::predict() output
+masterUnmarkedPredDet <- list() # unmarked::predict() output
 
+for (i in 1:length(communities)) {
+  
+  estimatedParameters <- list()
+  unmarkedPredOcc <- list()
+  unmarkedPredDet <- list()
+  
+  # stations info
+  stations <- read.csv(paste0(communities[i], "/Data/", communitiesAbrv[i], "StationsFormatted.csv"))
+  cameraRecords <- read.csv(paste0(communities[i], "/Data/", communitiesAbrv[i], "IndependentRecordsFormatted.csv"))
+  
+  # import site covariates for each community
+  if (communities[i] == "Sinangoe"){
+    siteCovariate <- data.frame(DistToComm = scale(stations$Community/1000)) # site covariates (scaled)
+    df <- data.frame(
+      DistToComm = siteCovariate$DistToComm
+    ) 
+    
+    for (j in 1:length(speciesNames)) { # for each species
+      # state = occupancy
+      unmarkedPredOcc[[j]] <- unmarked::predict(masterBestModsFitLists[[i]][[j]], 
+                                                type = 'state', new = df, appendData = TRUE)
+      # det = detection
+      bestModel <- occu(formula(masterTopModels[[i]][[j]]$ModelName[1]), 
+                                   ufoMasterList[[i]][[j]])
+      nullDetPred <- backTransform(bestModel, "det")
+      
+      # average
+      estimatedParameters[[j]] <- data.frame(avgOccupancy = mean(unmarkedPredOcc[[j]]$Predicted),
+                                             avgOccupancySE = mean(unmarkedPredOcc[[j]]$SE),
+                                             occupancyRange = range(unmarkedPredOcc[[j]]$Predicted),
+                                             avgDetection = nullDetPred@estimate,
+                                             avgDetectionSE = SE(nullDetPred),
+                                             detectionRange = NA)  
+    }
+    
+  } else if (communities[i] == "Zabalo") {
+    load('Zabalo/Data/R Objects/siteCovs2018.RData') # loads 'siteCovariate'
+    df <- data.frame(
+      Habitat = siteCovariate$Habitat,
+      HuntingIntensity = siteCovariate$HuntingIntensity,
+      Trail.Distance = siteCovariate$Trail.Distance,
+      Effort = siteCovariate$Effort
+    ) 
+    
+    for (j in 1:length(speciesNames)) { # for each species
+      # state = occupancy
+      unmarkedPredOcc[[j]] <- unmarked::predict(masterBestModsFitLists[[i]][[j]], 
+                                                type = 'state', new = df, appendData = TRUE)
+      # det = detection
+      unmarkedPredDet[[j]] <- unmarked::predict(masterBestModsFitLists[[i]][[j]], 
+                                                type = 'det', new = df, appendData = TRUE)
+      
+      # average
+      estimatedParameters[[j]] <- data.frame(avgOccupancy = mean(unmarkedPredOcc[[j]]$Predicted),
+                                             avgOccupancySE = mean(unmarkedPredOcc[[j]]$SE),
+                                             occupancyRange = range(unmarkedPredOcc[[j]]$Predicted),
+                                             avgDetection = mean(unmarkedPredDet[[j]]$Predicted),
+                                             avgDetectionSE = mean(unmarkedPredDet[[j]]$SE),
+                                             detectionRange = range(unmarkedPredDet[[j]]$Predicted))  
+    }
+    
+  } else {
+    
+    # for Siona and Siekopai who don't have covariates
+    siteCovariate <- NULL # no covariates for remaining communities 
+    df <- NULL # not sure what to do with this since we don't have covariates for other communities
+    
+    # calculate occupancy and detection estimates
+    for (j in 1:length(speciesNames)){
+      bestModel <- occu(~1 ~1, ufoMasterList[[i]][[j]])
+      nullOccPred <- backTransform(bestModel, "state")
+      nullDetPred <- backTransform(bestModel, "det")
+      
+      # average
+      estimatedParameters[[j]] <- data.frame(avgOccupancy = nullOccPred@estimate,
+                                             avgOccupancySE = SE(nullOccPred),
+                                             occupancyRange = NA,
+                                             avgDetection = nullDetPred@estimate,
+                                             avgDetectionSE = SE(nullDetPred),
+                                             detectionRange = NA)  
+    }
 
+    
+  }
+  names(estimatedParameters) <- speciesNames
+  
+
+  masterEstimatedParameters[[i]] <- estimatedParameters
+  masterUnmarkedPredOcc[[i]] <- unmarkedPredOcc
+  masterUnmarkedPredDet[[i]] <- unmarkedPredDet
+  
+} 
+  
+names(masterEstimatedParameters) <- communities
+names(masterUnmarkedPredOcc) <- communities
+names(masterUnmarkedPredDet) <- communities
 
 
 
