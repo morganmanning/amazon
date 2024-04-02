fa5e49a9d43902721c71bb17fd638c0be3416b4f
morganmanning <morgan5761@gmail.com>
2024-04-02 13:36:44 -0400

    finished functional traits file and fixed species misspellings

--- RawDataFormatting/formattingRawDates.R
+++ RawDataFormatting/formattingRawDates.R
@@ -1,13 +1,15 @@
 setwd("~/Documents/amazon/RawDataFormatting")
-
+require(lubridate)
 ##### Cleaning up dates, etc. on raw datasets 
 
 ### Zabalo
-Data <- read.csv("Zabalo/ZABIndependentRecords.csv") # just independent records
-Traps <- read.csv("Zabalo/ZABStations.csv") 
+Data <- read.csv("../Zabalo/Data/ZABIndependentRecords.csv") # just independent records
+Traps <- read.csv("../Zabalo/Data/ZABStations.csv") 
 
 Data$Species <- gsub("-", " ", Data$Species)
 Data$Species <- gsub("Pecari tajacu", "Dicotyles tajacu", Data$Species)
+Data$Species <- gsub("Hadroscirus spadiceus", "Hadrosciurus spadiceus", Data$Species)
+Data$Species <- gsub("Tinamu major", "Tinamus major", Data$Species)
 Data$DateTimeOriginal <- parse_date_time(Data$DateTimeOriginal, "%m/%d/%y %H:%M")
 Data$Date <- as.Date(Data$Date, format = "%m/%d/%y")
 Data$DateTimeOriginal <- with(Data, ymd(Date) + hms(Time))
@@ -19,33 +21,27 @@ Traps$Problem2_from <- parse_date_time(Traps$Problem2_from, "%m/%d/%y")
 Traps$Problem2_to <- parse_date_time(Traps$Problem2_to, "%m/%d/%y")
 Traps$Problem3_from <- parse_date_time(Traps$Problem3_from, "%m/%d/%y")
 Traps$Problem3_to <- parse_date_time(Traps$Problem3_to, "%m/%d/%y")
-Traps <- subset(Traps, Setup_date != Retrieval_date)
+# Traps <- subset(Traps, Setup_date != Retrieval_date)
 
 write.csv(Data, "../Zabalo/Data/ZABIndependentRecordsFormatted.csv")
-write.csv(Data, "Zabalo/ZABIndependentRecordsFormatted.csv")
-
-write.csv(Traps, "Zabalo/ZABStationsFormatted.csv")
 write.csv(Traps, "../Zabalo/Data/ZABStationsFormatted.csv")
 
 ### Sinangoe
-Data <- read.csv("Sinangoe/SGEIndependentRecords.csv") # just independent records
-Traps <- read.csv("Sinangoe/SGEStations.csv") 
+Data <- read.csv("../Sinangoe/Data/SGEIndependentRecords.csv") # just independent records
+Traps <- read.csv("../Sinangoe/Data/SGEStations.csv") 
 
 Traps$Setup_date <-as.Date(Traps$Setup_date, tryFormats = c("%m/%d/%y", "%d/%m/%Y", "%m/%d/%Y"))
 Traps$Retrieval_date <- as.Date(Traps$Retrieval_date, tryFormats = c("%m/%d/%y", "%d/%m/%Y", "%m/%d/%Y"))
 Traps$Problem1_from <- parse_date_time(Traps$Problem1_from, c("%d/%m/%Y", "%d/%m/%y", "%m/%d/%Y", "%m/%d/%y"))
 Traps$Problem1_to <- parse_date_time(Traps$Problem1_to, c("%d/%m/%Y", "%d/%m/%y", "%m/%d/%Y", "%m/%d/%y"))
 
-write.csv(Data, "Sinangoe/SGEIndependentRecordsFormatted.csv")
 write.csv(Data, "../Sinangoe/Data/SGEIndependentRecordsFormatted.csv")
-
-write.csv(Traps, "Sinangoe/SGEStationsFormatted.csv")
 write.csv(Traps, "../Sinangoe/Data/SGEStationsFormatted.csv")
 
 
 ### Siona
-Data <- read.csv("Siona/SNAIndependentRecords.csv") # just independent records
-Traps <- read.csv("Siona/SNAStations.csv") 
+Data <- read.csv("../Siona/Data/SNAIndependentRecords.csv") # just independent records
+Traps <- read.csv("../Siona/Data/SNAStations.csv") 
 
 Traps$Setup_date <-as.Date(Traps$Setup_date, "%d/%m/%Y")
 Traps$Retrieval_date <- as.Date(Traps$Retrieval_date, "%d/%m/%Y")
@@ -54,27 +50,21 @@ Traps$Problem1_to <- parse_date_time(Traps$Problem1_to, "%d/%m/%Y")
 Traps$Problem2_from <- parse_date_time(Traps$Problem2_from, "%d/%m/%Y")
 Traps$Problem2_to <- parse_date_time(Traps$Problem2_to, "%d/%m/%Y")
 
-write.csv(Data, "Siona/SNAIndependentRecordsFormatted.csv")
 write.csv(Data, "../Siona/Data/SNAIndependentRecordsFormatted.csv")
-
-write.csv(Traps, "Siona/SNAStationsFormatted.csv")
 write.csv(Traps, "../Siona/Data/SNAStationsFormatted.csv")
 
 
 
 ### Siekopai
-Data <- read.csv("Siekopai/SKPIndependentRecords.csv") # just independent records
-Traps <- read.csv("Siekopai/SKPStations.csv")
+Data <- read.csv("../Siekopai/Data/SKPIndependentRecords.csv") # just independent records
+Traps <- read.csv("../Siekopai/Data/SKPStations.csv")
 
 Traps$Setup_date <-as.Date(Traps$Setup_date, tryFormats = c("%m/%d/%y", "%d/%m/%Y", "%m/%d/%Y"))
 Traps$Retrieval_date <- as.Date(Traps$Retrieval_date, tryFormats = c("%m/%d/%y", "%d/%m/%Y", "%m/%d/%Y"))
 Traps$Problem1_from <- parse_date_time(Traps$Problem1_from, c("%d/%m/%Y", "%d/%m/%y", "%m/%d/%Y", "%m/%d/%y"))
 Traps$Problem1_to <- parse_date_time(Traps$Problem1_to, c("%d/%m/%Y", "%d/%m/%y", "%m/%d/%Y", "%m/%d/%y"))
 
-write.csv(Data, "Siekopai/SKPIndependentRecordsFormatted.csv")
 write.csv(Data, "../Siekopai/Data/SKPIndependentRecordsFormatted.csv")
-
-write.csv(Traps, "Siekopai/SKPStationsFormatted.csv")
 write.csv(Traps, "../Siekopai/Data/SKPStationsFormatted.csv")
 
 
