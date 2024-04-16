7399c976514bc688a36d47a366c296690a2024fb
Morgan Manning <morgan5761@gmail.com>
2024-04-16 15:23:37 -0400

    finished clustering

--- Global/iQAAPclustering.R
+++ Global/iQAAPclustering.R
@@ -13,6 +13,7 @@ require(cluster)
 require(lubridate)
 require(camtrapR)
 require(unmarked)
+require(ggplot2)
 
 # load in the necessary data
 functionalTraits <- read.csv("speciesAttributesManualInput.csv")
@@ -20,7 +21,7 @@ Data <- read.csv("AllIndependentRecordsFormatted.csv")
 Traps <- read.csv("AllStationsFormatted.csv")
 Data$DateTimeOriginal <- parse_date_time(Data$DateTimeOriginal, c("%Y-%m-%d", "%Y-%m-%d %H:%M:%S"))
 
-
+load('../../Zabalo/Data/R Objects/siteCovs2018.RData')
 
 ################################################################################
 # ----------------------------- CLUSTERING ------------------------------------#
@@ -95,7 +96,7 @@ Operation <- cameraOperation(CTtable = Traps,
 # detection matrices for each cluster for each number of clusters
 clusterRange <- 3:(nrow(functionalTraits)-1) # 3 because three classes (mammals, birds, reptile)
 # Number of clusters 'k' must be in {1,2, .., n-1}; hence n >= 2
-
+ 
 for (k in 1:length(clusterRange)) {
   # k-means clustering
   nClusters <- clusterRange[k]
@@ -104,16 +105,12 @@ for (k in 1:length(clusterRange)) {
   clusterDF <- as.data.frame(clusters)
   clusterDF <- data.frame(species = gsub("_", " ", rownames(clusterDF)),
                           cluster = clusterDF$clusters)
-  
   for (j in 1:nClusters) { # make detection matrices
     # pull each cluster
     clusterSpecies <- subset(clusterDF, cluster == j)[,1]
-    
     for (i in 1:length(clusterSpecies)) {
-      
       # occasion length
-      occasion = 2
-      
+      occasion = 10
       # species detection histories for occupancy analyses
       DetHis = detectionHistory(recordTable = Data,
                                 camOp = Operation,
@@ -130,21 +127,15 @@ for (k in 1:length(clusterRange)) {
                                 scaleEffort = FALSE,
                                 #maxNumberDays = 90, #need to think about this
                                 species = clusterSpecies[i]) #change species here
-      
-      justDetHis[[i]] <- DetHis[["detection_history"]]
-      
-    }
-    
-    summedMatrices <- Reduce("+", justDetHis)
+            justDetHis[[i]] <- DetHis[["detection_history"]]
+          }
+        summedMatrices <- Reduce("+", justDetHis)
     summedMatrices[summedMatrices > 0] <- 1
     clusterDetHis[[j]] <- summedMatrices # matrices for each cluster
-    
-  }
-  
-  allClusterDetHis[[k]] <- clusterDetHis # matrices for each cluster at each grouping factor
+      }
+    allClusterDetHis[[k]] <- clusterDetHis # matrices for each cluster at each grouping factor
   print(paste0("Just finished clustering by ", clusterRange[k], " out of ", max(clusterRange), " :)"))
-  
-}
+  }
 
 
 
@@ -152,32 +143,359 @@ for (k in 1:length(clusterRange)) {
 # --------------------------- OCCUPANCY MODELS --------------------------------#
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 
-# run single-species, single-season occupancy model for each cluster
+# prep dataframe for estimate input
+estimates <- data.frame()
 
-occupancyModelList <- as.list(rep(0, length(allClusterDetHis)))
+# run single-species, single-season occupancy model for each cluster
+occupancyModelList <- list()
+middleman <- list()
 
 for (i in 1:length(allClusterDetHis)){ # take the clusters that are clustered by __
-  for (j in 1:length(allClusterDetHis[[i]])){ # take each cluster and run null occupancy on it
-    ufo <- unmarkedFrameOccu(allClusterDetHis[[i]][[j]],
-                             siteCovs = NULL,
-                             obsCovs = NULL)
-    occupancyModelList[[i]][[j]] <- occu(~1 ~1, ufo, 
-                                         control = 10000,
-                                         starts = c(0, 0)) # starting values for parameters
-  }
+    ## make dataframe to fill in with estimates
+    perClustering <- data.frame(clusterBy = rep(clusterRange[i], times = clusterRange[i]),
+                        clusterNumber = NA,
+                        occupancyEstimate = NA,
+                        occupancySE = NA,
+                        detectionEstimate = NA,
+                        detectionSE = NA)
+    
+    for (j in 1:length(allClusterDetHis[[i]])){ # take each cluster and run null occupancy on it
+        ## run null model for each cluster
+        cluster <- allClusterDetHis[[i]][[j]]
+        ufo <- unmarkedFrameOccu(cluster,
+                                 siteCovs = NULL,
+                                 obsCovs = NULL)
+        output <- occu(~1 ~1, ufo, 
+                       control = 10000,
+                       starts = c(0, 0)) # starting values for parameters
+        middleman[[j]] <- output
+
+        ## fill in estimates DF
+        occPred <- backTransform(output, "state")
+        detPred <- backTransform(output, "det")
+        #perClustering$clusterBy[j] <- clusterRange[i]
+        perClustering$clusterNumber[j] <- j
+        perClustering$occupancyEstimate[j] <- occPred@estimate
+        perClustering$occupancySE[j] <- SE(occPred)
+        perClustering$detectionEstimate[j] <- detPred@estimate
+        perClustering$detectionSE[j] <- SE(detPred)
+    }
+    estimates <- rbind(estimates, perClustering)
+    occupancyModelList[[i]] <- middleman # have to do this or get a vector too long error
   print(paste0("Just finished loop ", i, " out of ", length(allClusterDetHis), " :)"))
 }
 
-## TESTING
-ufo <- unmarkedFrameOccu(allClusterDetHis[[54]][[1]],
-                         siteCovs = NULL,
-                         obsCovs = NULL)
-occupancyModelList[[54]][[1]] <- occu(~1 ~1, ufo, 
-                                     control = 10000,
-                                     starts = c(0, 0))
-dim(allClusterDetHis[[54]][[1]]) # 120x66
-# ERROR: long vectors not supported yet: /Volumes/Builds/R4/R-4.3.2/src/main/subassign.c:1841
 
+################################################################################
+# -------------------------- PLOTTING ESTIMATES -------------------------------#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+# average detection and occupancy per clustering
+averagedByClustering <- estimates |>
+    group_by(clusterBy) |>
+    mutate(averagedOccupancy = mean(occupancyEstimate),
+           averagedOccupancySE = mean(occupancySE, na.rm = TRUE),
+           averagedDetection = mean(detectionEstimate),
+           averagedDetectionSE = mean(detectionSE, na.rm = TRUE)) |>
+    mutate(averagedOccupancySE = replace(averagedOccupancySE, is.na(occupancySE), 1),
+           averagedDetectionSE = replace(averagedDetectionSE, is.na(detectionSE), 1))
+
+# plotting averaged occupancy estimates
+ggplot(averagedByClustering, aes(x = clusterBy,  y = averagedOccupancy)) +
+    geom_ribbon(aes(ymin = averagedOccupancy - averagedOccupancySE,
+                    ymax = averagedOccupancy + averagedOccupancySE),
+                alpha = 0.5, fill = "lightgreen") +
+    geom_line(color = "darkgreen") +
+    geom_point(color = "forestgreen") +
+    coord_cartesian(ylim = (c(0.75,1))) +
+    scale_x_discrete(name ="Number of clusters", 
+                     limits=seq(min(clusterRange), max(clusterRange), by = 4)) +
+    ylab("Averaged occupancy estimate") +             
+    theme_bw()
+
+# plotting averaged detection estimates
+ggplot(averagedByClustering, aes(x = clusterBy, y = averagedDetection)) +
+    geom_ribbon(aes(ymin = averagedDetection - averagedDetectionSE,
+                    ymax = averagedDetection + averagedDetectionSE),
+                alpha = 0.5, fill = "lightblue")  +
+    geom_point(color = "steelblue") +
+    geom_line(color = "steelblue") +
+    coord_cartesian(ylim = (c(0.45,.75))) +
+    scale_x_discrete(name ="Number of clusters", 
+                     limits=seq(min(clusterRange), max(clusterRange), by = 4)) +
+    ylab("Averaged detection estimate") +             
+    theme_bw()
+
+# plotting occupancy estimates
+ggplot(estimates, aes(x = clusterBy, y = occupancyEstimate)) +
+    geom_point() +
+    #geom_line() +
+    geom_ribbon(aes(ymin = occupancyEstimate - occupancySE,
+                    ymax = occupancyEstimate + occupancySE), alpha = 0.5) +
+    theme_bw()
+
+# plotting detection estimates
+ggplot(estimates, aes(x = as.factor(clusterBy), y = detectionEstimate)) +
+    geom_point() +
+    #geom_line() +
+    geom_ribbon(aes(ymin = detectionEstimate - detectionSE,
+                    ymax = detectionEstimate + detectionSE), alpha = 0.5) +
+    theme_bw()
+
+
+
+
+
+
+
+################################################################################
+# -------------------------- BROCKET CASE STUDY -------------------------------#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+brocketSpecies <- c("Mazama americana", "Mazama gouazoubira",
+                    "Mazama nemorivaga", "Mazama sp.")
+
+# detection matrices for all brockets and all brockets
+brocketMatrices <- list()
+for (i in 1:length(brocketSpecies)) {
+      # occasion length
+      occasion = 10
+      # species detection histories for occupancy analyses
+      DetHis = detectionHistory(recordTable = Data,
+                                camOp = Operation,
+                                output = "binary", # binary or count
+                                stationCol = "Station",
+                                speciesCol = "Species",
+                                recordDateTimeCol = "DateTimeOriginal",
+                                recordDateTimeFormat = "%Y-%m-%d %H:%M:%S",
+                                day1 = "Station",
+                                occasionLength = occasion,
+                                datesAsOccasionNames = FALSE,
+                                timeZone = "America/Guayaquil",
+                                includeEffort = TRUE,
+                                scaleEffort = FALSE,
+                                #maxNumberDays = 90, #need to think about this
+                                species = brocketSpecies[i]) #change species here
+            brocketMatrices[[i]] <- DetHis[["detection_history"]]
+          }
+summedMatrices <- Reduce("+", justDetHis)
+summedMatrices[summedMatrices > 0] <- 1
+brocketMatrices[[5]] <- summedMatrices # matrix for all brockets
+names(brocketMatrices) <- c(brocketSpecies, "All brockets")
+
+# run single-species, single-season null occupancy model for each cluster
+occuBrocket <- list()
+brocketEstimates  <- data.frame(species = c(brocketSpecies, "All brockets"),
+                        occupancyEstimate = NA,
+                        occupancySE = NA,
+                        detectionEstimate = NA,
+                        detectionSE = NA)
+for (i in 1:length(brocketMatrices)){ # take each cluster and run null occupancy on it
+        ## run null model for each cluster
+        brocketN <- brocketMatrices[[i]]
+        ufo <- unmarkedFrameOccu(brocketN,
+                                 siteCovs = NULL,
+                                 obsCovs = NULL)
+        output <- occu(~1 ~1, ufo, 
+                       control = 10000,
+                       starts = c(0, 0)) # starting values for parameters
+        occuBrocket[[j]] <- output
+        ## fill in estimates DF
+        occPred <- backTransform(output, "state")
+        detPred <- backTransform(output, "det")
+        brocketEstimates$occupancyEstimate[i] <- occPred@estimate
+        brocketEstimates$occupancySE[i] <- SE(occPred)
+        brocketEstimates$detectionEstimate[i] <- detPred@estimate
+        brocketEstimates$detectionSE[i] <- SE(detPred)
+    }
+brocketEstimates
+
+# plot null occupancy estimates
+ggplot(brocketEstimates, aes(x = c(as.character(gsub(" ", "\n", brocketEstimates$species))),
+                             y = occupancyEstimate)) +
+    geom_errorbar(aes(ymin = occupancyEstimate - occupancySE,
+                    ymax = occupancyEstimate + occupancySE),
+               width = 0.2, color  = "darkgreen") +
+    geom_line(color = "darkgreen") +
+    geom_point(color = "darkgreen", size = 2) +
+    ylab("Occupancy estimate") +
+    xlab("Brocket species") +
+    theme_bw()
+
+# plot null detection estimates
+ggplot(brocketEstimates, aes(x = c(as.character(gsub(" ", "\n", brocketEstimates$species))),
+                             y = detectionEstimate)) +
+    geom_errorbar(aes(ymin = detectionEstimate - detectionSE,
+                    ymax = detectionEstimate + detectionSE),
+               width = 0.2, color  = "dodgerblue") +
+    geom_line(color = "dodgerblue") +
+    geom_point(color = "dodgerblue", size = 2) +
+    ylab("Detection estimate") +
+    scale_x_discrete(name = "Brocket species") +
+    theme_bw()
+
+
+# run single-species, single-season occupancy model with covariates
+# prediction dataframes
+communityPrediction <- data.frame(Community = c("Zabalo", "Siekopai", "Sinangoe", "Siona"),
+                                  Year = as.factor(2022))
+communityPrediction$Community <- factor(communityPrediction$Community,
+                                        levels = c("Zabalo", "Siekopai", "Sinangoe", "Siona"))
+yearPrediction <- data.frame(Community = "Siekopai",
+                             Year = as.factor(unique(year(Data$DateTimeOriginal))))
+
+# run models
+brocketCommPred <- list()
+brocketYearPred <- list()
+occuCovariatesBrocket <- list()
+for (i in 1:length(brocketMatrices)){ # take each cluster and run null occupancy on it
+    
+        ## run model for each species
+    brocketN <- brocketMatrices[[i]]
+
+    ## site covariates
+    siteCovariates <- data.frame(Station = rownames(brocketN),
+                                 Community = NA,
+                                 Year = NA)
+    siteCovariates$Community <- ifelse(grepl('^ZAB', siteCovariates$Station), 'Zabalo',
+                                ifelse(grepl('^SKP', siteCovariates$Station), 'Siekopai',
+                                ifelse(grepl('^SGE', siteCovariates$Station), 'Sinangoe', "Siona")))
+    siteCovariates$Community <- factor(siteCovariates$Community,
+                                       levels = c("Zabalo", "Siekopai", "Sinangoe", "Siona"))
+    siteCovariates$Year <- ifelse(siteCovariates$Community == "Zabalo", "2018", "2022")
+    
+        ufo <- unmarkedFrameOccu(brocketN,
+                                 siteCovs = siteCovariates,
+                                 obsCovs = NULL)
+        output <- occu(~1 ~Community, ufo, 
+                       control = 10000) 
+        occuCovariatesBrocket[[j]] <- output
+    # occupancy prediction
+    brocketCommPred[[i]] <- unmarked::predict(fitList(output), type = "state",
+                                              new = communityPrediction, append = TRUE)
+    #brocketYearPred[[i]] <- unmarked::predict(fitList(output), type = "state",
+                                              #new = yearPrediction, append = TRUE)
+    }
+
+names(brocketCommPred) <- names(brocketMatrices)
+names(brocketYearPred) <- names(brocketMatrices)
+
+# make dataframe for community predictions
+brocketCommPredDF <- dplyr::bind_rows(brocketCommPred) 
+brocketCommPredDF$Species <- rep(names(brocketCommPred), each = nrow(brocketCommPred[[1]]))
+brocketCommPredDF
+
+# make dataframe for year predictions
+# brocketYearPredDF <- dplyr::bind_rows(brocketYearPred) 
+# brocketYearPredDF$Species <- rep(names(brocketYearPred), each = nrow(brocketYearPred[[1]]))
+# brocketYearPredDF
+
+# plot occupancy by species and community
+ggplot(brocketCommPredDF, aes(x = c(as.character(gsub(" ", "\n", brocketCommPredDF$Species))),
+                             y = Predicted, color = Community)) +
+    geom_errorbar(aes(ymin = Predicted - SE,
+                    ymax = Predicted + SE),
+               width = 0.2) +
+    geom_point(size = 2) +
+    ylab("Occupancy estimate") +
+    scale_x_discrete(name = "Brocket species") +
+    scale_color_manual(values = c("darkgreen", "forestgreen","yellowgreen","goldenrod")) +
+    theme_bw()
+        
+
+## other potentially useful stats for paper
+table(Data$CommunityName)
+
+
+
+
+
+
+
+
+
+################################################################################
+# ----------------------- ABUNDANCE AND DIVERSITY -----------------------------#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+# remove all unknown species
+noUnknownsSGE <- Data[(Data$Species != "N/D N/D" & Data$CommunityName == "Sinangoe"),]
+noUnknownsZAB <- Data[(Data$Species != "NAN NAN" & Data$Species != "NA NA" & Data$CommunityName == "Zabalo"),]
+noUnknownsSNA <- Data[(Data$Species != "N/D N/D" & Data$CommunityName == "Siona"),]
+noUnknownsSKP <- Data[(Data$Species != "N/D N/D" & Data$CommunityName == "Siekopai"),]
+
+# per station
+  # Sinangoe
+siteDiversitySGE <- noUnknownsSGE %>%
+  group_by(Station, Species) %>%
+  summarise(abundance = n()) 
+
+siteDiversitySGE <- siteDiversitySGE %>%
+  group_by(Station) %>%
+  summarise(N=sum(abundance),
+            shannonDiversity = -sum((abundance/sum(abundance))*log(abundance/sum(abundance))),
+            simpsonDiversity = 1-sum((abundance/sum(abundance))^2))
+
+# Zábalo
+siteDiversityZAB <- noUnknownsZAB %>%
+  group_by(Station, Species) %>%
+  summarise(abundance = n()) 
+
+siteDiversityZAB <- siteDiversityZAB %>%
+  group_by(Station) %>%
+  summarise(N=sum(abundance),
+            shannonDiversity = -sum((abundance/sum(abundance))*log(abundance/sum(abundance))),
+            simpsonDiversity = 1-sum((abundance/sum(abundance))^2))
+
+# Siona
+siteDiversitySNA <- noUnknownsSNA %>%
+  group_by(Station, Species) %>%
+  summarise(abundance = n()) 
+
+siteDiversitySNA <- siteDiversitySNA %>%
+  group_by(Station) %>%
+  summarise(N=sum(abundance),
+            shannonDiversity = -sum((abundance/sum(abundance))*log(abundance/sum(abundance))),
+            simpsonDiversity = 1-sum((abundance/sum(abundance))^2))
+
+# Siekopai
+siteDiversitySKP <- noUnknownsSKP %>%
+  group_by(Station, Species) %>%
+  summarise(abundance = n()) 
+
+siteDiversitySKP <- siteDiversitySKP %>%
+  group_by(Station) %>%
+  summarise(N=sum(abundance),
+            shannonDiversity = -sum((abundance/sum(abundance))*log(abundance/sum(abundance))),
+            simpsonDiversity = 1-sum((abundance/sum(abundance))^2))
+
+# per community
+wholeDiversitySGE <- noUnknownsSGE %>%
+  group_by(Species) %>%
+  summarise(abundance = n()) %>%
+  mutate(Community = "Sinangoe", PercentNaturalArea = 0.7660)
+wholeDiversityZAB <- noUnknownsZAB %>%
+  group_by(Species) %>%
+  summarise(abundance = n()) %>%
+  mutate(Community = "Zábalo", PercentNaturalArea = 0.9356)
+wholeDiversitySNA <- noUnknownsSNA %>%
+  group_by(Species) %>%
+  summarise(abundance = n()) %>%
+  mutate(Community = "Siona", PercentNaturalArea = 0.7538)
+wholeDiversitySKP <- noUnknownsSKP %>%
+  group_by(Species) %>%
+  summarise(abundance = n()) %>%
+  mutate(Community = "Siekopai", PercentNaturalArea = 0.8071)
+
+# abundance and diversity for all communities
+communityAbundance <- rbind(wholeDiversityZAB, wholeDiversitySKP, wholeDiversitySGE, wholeDiversitySNA)
+communityDiversity <- communityAbundance %>%
+    group_by(Community, PercentNaturalArea) %>%
+  summarise(nIndiv=sum(abundance),
+            nSpecies = length(unique(Species)),
+            shannonIndex = -sum((abundance/sum(abundance))*log(abundance/sum(abundance))),
+            simpsonIndex = 1-sum((abundance/sum(abundance))^2)) 
+communityDiversity
 
 
 
@@ -186,8 +504,8 @@ dim(allClusterDetHis[[54]][[1]]) # 120x66
 
 # TO DO:
   # X include all of above in for loop to run all of it for each number of clusters
-  # - run occupancy on each cluster for each cluster delineation
-  # - plot occupancy predictions for each of said occupancy models
+  # x run occupancy on each cluster for each cluster delineation
+  # X plot occupancy predictions for each of said occupancy models
 
 
 
