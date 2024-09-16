f5addabede016309912f04dcf46b1e449246fcb5
Morgan Manning <morgan5761@gmail.com>
2024-09-16 14:04:02 -0400

    multispecies modeling

--- Global/multiSpeciesOccupancy.R
+++ Global/multiSpeciesOccupancy.R
@@ -139,13 +139,40 @@ for (i in 1:length(species)) {
   names(detection)[i] <- casualNames[i]
 }
 
+# identify empty rows (i.e. stations with no detections)
+empty_rows <- vector()
+for (i in 1:length(detection)){
+  allNArow <- c(as.numeric(which(rowSums(is.na(detection[[i]]))==ncol(detection[[i]]))))
+  empty_rows <- append(empty_rows, allNArow)
+}
+
+# remove empty rows if they're empty in all species 
+detectionWithoutBlanks <- list()
+if(all(table(empty_rows) == length(species)) & 
+all(covariates$Station == rownames(detection[[1]]))) { # if every species has the same empty rows
+  rowsToCull <- unique(empty_rows)
+  for (i in 1:length(detection)){
+    detectionWithoutBlanks[[i]] <- detection[[i]][-rowsToCull,]
+  }
+  covariatesWithoutBlanks <- covariates[-rowsToCull,]
+  names(detectionWithoutBlanks) <- names(detection)
+}
+str(detectionWithoutBlanks)
+nrow(covariatesWithoutBlanks)
+
+
 
 # should i do time step consolidation here?
 
 # make the unmarked frame
-umf <- unmarkedFrameOccuMulti(y = detection, siteCovs = covariates, maxOrder = 2)
+umf <- unmarkedFrameOccuMulti(y = detectionWithoutBlanks, siteCovs = covariatesWithoutBlanks, maxOrder = 2)
 summary(umf)
 str(umf)
+
+
+
+
+
 
 
 
@@ -162,60 +189,131 @@ str(umf)
 # Distance to a water source (m)
 
 match_detection <- c("Community")
-match_occupancy <- c("Community", "Rainfall", "percentNatural", "DistToWater", "Temperature")
+match_occupancy <- c("Rainfall", "percentNatural", "DistToWater", "Temperature")
+# excluded "Community" from occupancy covariates bc it correlated with percentNatural (per chisq.test())
 
 
-############### BEST DETECTION MODEL PER SPECIES
+############# BEST OCCUPANCY MODEL PER SPECIES USING THE BEST DETECTION MODEL
 # every possible combination of variables
-combos <- sapply(seq(length(match_detection)), function(k) {
-  as.list(as.data.frame(combn(x = match_detection, m = k)))
+combos <- sapply( seq(length(match_occupancy)), function(i) {
+    as.list(as.data.frame(combn( x = match_occupancy, m = i)))
 })
 combos <- unlist(combos, recursive=FALSE)
-
+  
 # all combinations of variables into formulas
 forms <- sapply(combos, function(x) paste("~ ", paste(x, collapse="+"), sep = ""))
-detectionFormulas <- as.vector(c(forms, "~ 1"))
-
-# get the best detection formula and stick with that for the occupancy formulas
-tempDF <- data.frame(detection = detectionFormulas,
-                     occupancy = "~ 1")
-allDetectionFormulas <- paste(tempDF$detection, tempDF$occupancy, sep = " ")
-bestDetectionModels <- data.frame(species = commonNames,
-                                  bestDetectionModel = NA)
-
-# see which detection model is the best with null occupancy
-for (i in 1:length(species)){
-  # make a df off all detection formulas and their AIC for each species
-  detectionMods <- list()
-  temp <- data.frame(Model = allDetectionFormulas,
-                     AIC = NA)
+forms <- as.vector(c(forms, "~ 1")) # add the null model
+occupancyFormulas <- forms
   
-  # run occu model for every detection formula per species per community to compare AIC
-  for(j in 1:length(allDetectionFormulas)) {
-    test <- occuMulti(stateformulas = rep("~1", length(species)), 
-                      detformulas = rep(detectionFormulas[[j]], length(species)), 
-                      data = umf, maxOrder = 1)
-    
-    
-    detectionMods[[m]] <- occu(formula(allDetectionFormulas[[m]]), ufoMasterList[[i]][[j]], 
-                               control = 10000, 
-                               starts = c(rep(0, length(test@opt$par)))) 
-    temp$AIC[m] <- detectionMods[[m]]@AIC
+############### COMBINE DETECTION WITH ALL POSSIBLE OCCUPANCY PREDICTORS AND MODEL
+  occupancyModelsList <- list()
+  for (j in 1:length(species)) {
+    df <- data.frame(detection = '~ Community',
+                     occupancy = occupancyFormulas)
+    occupancyModelsList[[j]] <- c(paste(df$detection, df$occupancy, sep = " "))
   }
-  # select the model with the lowest AIC
-  bestDetectionModels$bestDetectionModel[j] <- detectionFormulas[which(temp$AIC == min(temp$AIC))]
   
-  # output is dataframe for community i with the best detection formulas for each species
+  # run occupancy unmarked model for all models
+  allModels <- list()
+  for (j in 1:length(species)) { # for all the species
+    occupancyMods <- list()
+    ufo <- unmarkedFrameOccu(detectionWithoutBlanks[[j]], 
+                             siteCovs = covariatesWithoutBlanks,
+                             obsCovs = NULL)
+    for(m in 1:length(occupancyModelsList[[j]])) {
+      test <- occu(formula(occupancyModelsList[[j]][m]), ufo)
+      occupancyMods[[m]] <- occu(formula(occupancyModelsList[[j]][m]), ufo, 
+                                 control = 10000, 
+                                 starts = c(rep(0, length(test@opt$par)))) 
+    }
+    names(occupancyMods) <- 1:length(occupancyMods)
+    allModels[[j]] <- occupancyMods
+    print(paste0("Finishing species #"), j, " out of ", length(species))
+  }
   
+  # Make a data frame to show the model names and their AICs
+  modelAICs <- list() # all models and their AICs
+  topModels <- list() # only models within 2 AIC of lowest AIC
+  for(j in 1:length(allModels)) { # for each species
+    df <- data.frame(ModelName = NA,
+                     AIC = NA,
+                     diffFromBest = NA)
+    for(m in 1:length(allModels[[j]])){
+      df[m,1]<- as.character(c(allModels[[j]][[m]]@formula))
+      df[m,2]<- allModels[[j]][[m]]@AIC
+    }
+    df <- df[order(df$AIC),]
+    df$diffFromBest <- df$AIC - min(df$AIC)
+    modelAICs[[j]] <- df
+    
+    # only the best
+    ANTM <- subset(df, diffFromBest <= 2)
+    topModels[[j]] <- ANTM
+  }
+  names(topModels) <- casualNames
+  
+  # create a new dataframe with list titles as a new column
+  topModels_df <- data.frame(Species = character(),
+                 ModelName = character(),
+                 AIC = numeric(),
+                 diffFromBest = numeric(),
+                 stringsAsFactors = FALSE)
+
+  for (j in 1:length(topModels)) {
+    species <- casualNames[j]
+    models <- topModels[[j]]
+    n <- nrow(models)
+    
+    # create a temporary dataframe for each species
+    temp_df <- data.frame(Species = rep(species, n),
+              ModelName = models$ModelName,
+              AIC = models$AIC,
+              diffFromBest = models$diffFromBest)
+    
+    # append the temporary dataframe to the main dataframe
+    topModels_df <- rbind(topModels_df, temp_df)
+  }
+
+# save the table
+kbl(topModels_df) %>%
+  kable_classic(font_size = 22, html_font = "TimesNewRoman") %>%
+  save_kable(file = "../Figures/MultispeciesModeling/7speciesBestModels.png", zoom = 2)
+
+# look at occupancy covariates
+topModels_df$OccupancyFormula <- gsub("~Community", "", topModels_df$ModelName)
+topModels_df[c("Species", "OccupancyFormula")]
+
+# extract the words/letter strings
+modelCovariates <- strsplit(gsub("[ ~]", "", topModels_df$OccupancyFormula), "[ +~]")
+names(modelCovariates) <- topModels_df$Species
+
+# make modelCovariates into a dataframe with the list titles as a new column
+modelCovariates_df <- data.frame(Species = character(),
+                 Covariates = character())
+
+for (i in 1:length(modelCovariates)) {  
+  # create a temporary dataframe for each species
+  temp_df <- data.frame(Species = rep(names(modelCovariates)[i], length(modelCovariates[[i]])),
+            Covariates = modelCovariates[[i]])
+  
+  # append the temporary dataframe to the main dataframe
+  modelCovariates_df <- rbind(modelCovariates_df, temp_df)
 }
 
+# pull the best covariates for each species and make a formula
+bestCovariates <- modelCovariates_df |> 
+  unique() |>
+  subset(Covariates  != "1") |>
+  mutate(ID = row_number()) |>
+  group_by(Species) |>
+  summarize(BestCovariates = paste("~", paste(Covariates, collapse = "+"))) 
+bestCovariates <- bestCovariates[match(casualNames, bestCovariates$Species), ]
+bestCovariates
 
 
+if (all(bestCovariates$Species == names(detectionWithoutBlanks))) {
+  
+  # use the formulas to run occuMulti (with ~Community as the detection formula)
 
-
-
-
-
-
-
+}
 
