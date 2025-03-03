b3f8593cbcdb278cf2a8557bbb176dad51f80fd9
Morgan Manning <morgan5761@gmail.com>
2025-03-03 13:54:35 -0500

    multispecies interactions!

--- Global/multispeciesPlotting.R
+++ Global/multispeciesPlotting.R
@@ -40,7 +40,7 @@ all_predictions$Species <- casualNames
 # plot null model predictions for occupancy
 plot(1:length(species), all_predictions$Predicted,
     # ylim = c(0.1, 0.4),
-    # xlim = c(0.5, 3.5),
+    xlim = c(0.5, 2.5),
     pch = 19, cex = 1.5, xaxt = "n",
     xlab = "", ylab = "Marginal occupancy and 95% CI",
     main = "Null model predictions for occupancy",
@@ -578,4 +578,280 @@ for (i in 1:length(covs)) {
         width = 8, height = 4
     )
 }
-# names(community_prediction_plots_list) <- covs
+
+
+
+
+################################################################################
+############### PLOTTING CONDITIONAL OCCUPANCY FOR ALL SPECIES #################
+################################################################################
+
+
+# PLOT BY COMMUNITY WITH THE COMMUNITY-ONLY MODEL
+# make a dataframe with column of species 1 and column of species 2 with all combinations of species
+communities <- c("Zabalo", "Remolino", "Sinangoe", "San Pablo", "Siona")
+community_conditional_occupancy <- data.frame(
+    Species1 = rep(expand.grid(commonNames, commonNames)$Var2, each = length(communities)),
+    Species2 = rep(expand.grid(commonNames, commonNames)$Var1, each = length(communities)),
+    Occupancy = NA,
+    SE = NA,
+    lower = NA,
+    upper = NA
+)
+community_conditional_occupancy <- community_conditional_occupancy[community_conditional_occupancy$Species1 != community_conditional_occupancy$Species2, ]
+community_conditional_occupancy$Community <- rep(communities, times = nrow(community_conditional_occupancy) / length(communities))
+
+# combine these the long way
+commPresent <- community_conditional_occupancy
+commAbsent <- community_conditional_occupancy
+commPresent$conditionalLabel <- paste0(commPresent$Species2," present")
+commAbsent$conditionalLabel <- paste0(commPresent$Species2, " absent")
+commPresent$conditional <- "Species2 Present"
+commAbsent$conditional <- "Species2 Absent"
+community_conditional_occupancy <- rbind(commPresent, commAbsent)
+
+
+
+
+# prediction data frame
+dfCommunity <- dfTemplate
+dfCommunity$Community <- rep(communities,
+    each = 100 / length(unique(communities))
+)
+dfCommunity <- dfCommunity[!duplicated(dfCommunity), ] # simplify
+speciesDictionary <- data.frame(
+    CasualName = casualNames,
+    CommonName = commonNames
+)
+
+
+# fill in community_conditional_occupancy
+for (i in 1:nrow(community_conditional_occupancy)) {
+    # establish variables
+    Species1 <- speciesDictionary$CasualName[speciesDictionary$CommonName == community_conditional_occupancy$Species1[i]]
+    Species2 <- speciesDictionary$CasualName[speciesDictionary$CommonName == community_conditional_occupancy$Species2[i]]
+    communityInQuestion <- community_conditional_occupancy$Community[i]
+
+    if (community_conditional_occupancy$conditional[i] == "Species2 Present") {
+        # prediction where both species present
+        pred <- c() # resets it so no accidental carryover
+        pred <- predict(community_mod_penalty,
+            type = "state",
+            species = Species1,
+            cond = Species2,
+            newdata = dfCommunity
+        )
+        pred$Community <- dfCommunity$Community
+        community_conditional_occupancy$Occupancy[i] <- pred$Predicted[pred$Community == communityInQuestion]
+        community_conditional_occupancy$SE[i] <- pred$SE[pred$Community == communityInQuestion]
+        community_conditional_occupancy$lower[i] <- pred$lower[pred$Community == communityInQuestion]
+        community_conditional_occupancy$upper[i] <- pred$upper[pred$Community == communityInQuestion]
+    } else if (community_conditional_occupancy$conditional[i] == "Species2 Absent") {
+        # prediction where Species2 is absent
+        pred <- c() # resets it so no accidental carryover
+        pred <- predict(community_mod_penalty,
+            type = "state",
+            species = Species1,
+            cond = paste0("-", Species2),
+            newdata = dfCommunity
+        )
+        pred$Community <- dfCommunity$Community
+        community_conditional_occupancy$Occupancy[i] <- pred$Predicted[pred$Community == communityInQuestion]
+        community_conditional_occupancy$SE[i] <- pred$SE[pred$Community == communityInQuestion]
+        community_conditional_occupancy$lower[i] <- pred$lower[pred$Community == communityInQuestion]
+        community_conditional_occupancy$upper[i] <- pred$upper[pred$Community == communityInQuestion]
+    } else 
+    next
+}
+
+# formatting to prep for plotting
+community_conditional_occupancy$Community <- gsub("Zabalo", "Zábalo", x = community_conditional_occupancy$Community)
+community_conditional_occupancy$Community <- factor(community_conditional_occupancy$Community,
+    levels = c("Zábalo", "Remolino", "Sinangoe", "San Pablo", "Siona")
+)
+community_conditional_occupancy$Species1 <- factor(community_conditional_occupancy$Species1, levels = commonNames) # so plotting doesn't alphabetize species
+community_conditional_occupancy$Species2 <- factor(community_conditional_occupancy$Species2, levels = commonNames) # so plotting doesn't alphabetize species
+
+# make labels per covariate
+colors <- c(
+    "Zábalo" = "darkgreen", "Remolino" = "forestgreen",
+    "Sinangoe" = "yellowgreen", "San Pablo" = "gold1", "Siona" = "darkgoldenrod3"
+)
+
+# make titles per potential species
+peccary <- ~ atop(paste("Collared peccary"), paste("(", italic("Pecari tajacu"), ")"))
+brocket <- ~ atop(paste("Brocket"), paste("(", italic("Mazama sp."), ")"))
+paca <- ~ atop(paste("Lowland paca"), paste("(", italic("Cuniculus paca"), ")"))
+trumpeter <- ~ atop(paste("Grey-winged trumpeter"), paste("(", italic("Psophia crepitans"), ")"))
+fourEyed <- ~ atop(paste("Brown four-eyed opossum"), paste("(", italic("Metachirus nudicaudatus"), ")"))
+agouti <- ~ atop(paste("Black agouti"), paste("(", italic("Dasyprocta fuliginosa"), ")"))
+armadillo <- ~ atop(paste("Nine-banded armadillo"), paste("(", italic("Dasypus novemcinctus"), ")"))
+tinamou <- ~ atop(paste("Great tinamou"), paste("(", italic("Tinamus major"), ")"))
+opossum <- ~ atop(paste("Common opossum"), paste("(", italic("Didelphis marsupialis"), ")"))
+ocelot <- ~ atop(paste("Ocelot"), paste("(", italic("Leopardus pardalis"), ")"))
+
+# rphylopic per species
+peccPic <- get_phylopic(uuid = get_uuid(name = "Pecari tajacu", n = 1))
+brockPic <- get_phylopic(uuid = get_uuid(name = "Mazama americana", n = 1))
+pacaPic <- get_phylopic(uuid = get_uuid(name = "Cuniculus paca", n = 1))
+trumpPic <- get_phylopic(uuid = get_uuid(name = "Psophia crepitans", n = 1))
+fourEyedPic <- get_phylopic(uuid = get_uuid(name = "Metachirus nudicaudatus", n = 1))
+agoutiPic <- get_phylopic(uuid = get_uuid(name = "Dasyprocta", n = 1))
+armadilloPic <- get_phylopic(uuid = get_uuid(name = "Dasypus novemcinctus", n = 1))
+tinamouPic <- get_phylopic(uuid = get_uuid(name = "Tinamus major", n = 1))
+opossumPic <- get_phylopic(uuid = get_uuid(name = "Didelphis", n = 1))
+ocelotPic <- get_phylopic(uuid = get_uuid(name = "Leopardus pardalis", n = 1))
+
+
+# plot interactions for each species
+for(i in 1:length(casualNames)){
+    dataSub <- subset(community_conditional_occupancy, Species1 == commonNames[i])
+    possibleInteractions <- unique(dataSub$Species2)
+
+    interactionPlots <- list()
+    for(j in 1:length(possibleInteractions)){
+    # subset the data again
+    dataSubSub <- subset(dataSub, Species2 == possibleInteractions[j])
+
+
+    # facet by Species2
+    dodge <- position_dodge(width = 0.3)
+    p <- ggplot(dataSub, aes(x = Community, y = Occupancy, color = conditionalLabel)) +
+    facet_wrap(~Species2) + 
+    geom_point(aes(color = conditionalLabel), position = dodge, size = 1.5) +
+    geom_errorbar(
+        aes(
+            ymin = lower,
+            ymax = upper,
+            color = conditionalLabel
+        ),
+        position = dodge, width = 0.15, linewidth = .5
+    ) +
+    labs(x = "Community", 
+    y = paste0("Conditional ", tolower(commonNames[i]), " occupancy probability")) +
+    ylim(c(0, 1)) +
+    theme_classic() +
+    theme(
+        text = element_text(family = "Times", colour = "black"),
+        axis.text = element_text(colour = "black"),
+        legend.title = element_blank(),
+        legend.position = "top",
+        axis.title.x = element_blank(),
+        panel.grid.major.y = element_line(color = "#cecece", linewidth = 0.2)
+    ) 
+    interactionPlots[[j]] <- p
+}
+
+# make the plot
+n <- length(interactionPlots)
+nCol <- floor(sqrt(n))
+p <- do.call("grid.arrange", c(interactionPlots, ncol = nCol))
+annotate_figure(p, top = text_grob(paste0(commonNames[i], " Interactions (Penalized community-only model)"),
+    face = "bold", size = 14
+))
+ggsave(paste0("../Figures/MultispeciesModeling/", casualNames[i], "_InteractionsByCommunity.png"),
+    width = 10, height = 8
+)
+}
+
+
+
+
+
+
+
+
+
+# plot it
+p <- ggplot(community_conditional_occupancy, aes(
+    x = Species1,
+    y = Present1_Present2,
+    color = Community
+)) +
+    geom_point(aes(color = Community), position = dodge, size = 1.5) +
+    geom_errorbar(
+        aes(
+            ymin = Predicted - SE,
+            ymax = Predicted + SE,
+            color = Community
+        ),
+        position = dodge, width = 0.15, linewidth = .5
+    ) +
+    # scale_color_manual(values = c("darkorange", "royalblue", "green3", "yellow3")) +
+    scale_color_manual(values = colors) +
+    scale_fill_manual(values = colors) +
+    scale_x_discrete(labels = c(paca, agouti)) +
+    labs(x = "Species", y = "Occupancy probability estimate (and SE)") +
+    ylim(c(0, 1)) +
+    theme_classic() +
+    theme(
+        text = element_text(family = "Times", colour = "black"),
+        axis.text = element_text(colour = "black"),
+        axis.text.x = element_text(angle = 45, vjust = 0.60),
+        legend.title = element_blank(),
+        legend.position = "top",
+        axis.title.x = element_blank(),
+        panel.grid.major.y = element_line(color = "#cecece", linewidth = 0.2)
+    ) +
+    add_phylopic(pacaPic, alpha = 0.2, x = 1.0, y = 0.05, ysize = 0.1) +
+    add_phylopic(agoutiPic, alpha = 0.2, x = 2.0, y = 0.05, ysize = 0.1)
+
+
+
+
+
+
+
+
+# TESTING
+# use the predict function without providing new data to predict by site, then extrapolate to by community
+test <- as.data.frame(predict(natural_mod_penalty,
+    type = "state",
+    species = "agouti",
+    cond = "paca"
+))
+test$Site <- rownames(combinedDetection)
+
+test$Community <- NA
+for(i in 1:nrow(test)){
+    # if a site starts with XYZ, then it's in the XYZ community
+    if (grepl("SNA", test$Site[i])){
+        test$Community[i] <- "Siona"
+    } else if(grepl("ZAB", test$Site[i])){
+        test$Community[i] <- "Zábalo"
+    } else if(grepl("SGE", test$Site[i])){
+        test$Community[i] <- "Sinangoe"
+    } else if(grepl("SKP", test$Site[i])){
+        test$Community[i] <- "Remolino"
+    }
+
+    # now assign San Pablo: SKP31-37, SNA3
+    if (test$Site[i] == "SNA3" | test$Site[i] == "SKP31" | test$Site[i] == "SKP32" |
+        test$Site[i] == "SKP33" | test$Site[i] == "SKP34" | test$Site[i] == "SKP35" |
+        test$Site[i] == "SKP36" | test$Site[i] == "SKP37") {
+        test$Community[i] <- "San Pablo"
+    }
+}
+
+# group by community and average the occupancy and calculate 95% CI
+test2 <- test %>%
+    group_by(Community) %>%
+    summarize(
+        SD = sd(Predicted),
+        Predicted = mean(Predicted), 
+        count = n()) %>%
+    mutate(
+        SE = SD / sqrt(count),
+        lower = Predicted - qt(0.975, df = count - 1) * SD / sqrt(count),
+        upper = Predicted + qt(0.975, df = count - 1) * SD / sqrt(count)
+    )
+test2
+
+
+# OPTION TWO: use the predict function with new data to predict by community
+
+
+
+
+
+
