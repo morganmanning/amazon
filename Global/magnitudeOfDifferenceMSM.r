752943ce3bbefc7dc29b9cb88ed084a5a2f0cf07
Morgan Manning <morgan5761@gmail.com>
2025-05-06 10:41:40 -0400

    issues defining N

--- Global/magnitudeOfDifferenceMSM.r
+++ Global/magnitudeOfDifferenceMSM.r
@@ -19,6 +19,7 @@ load("R Objects/pacaagoutiMSM_byCommunity.RData")
 paca_agouti <- community_conditional_occupancy
 load("R Objects/ocelotagoutiMSM_byCommunity.RData")
 ocelot_agouti <- community_conditional_occupancy
+Traps <- read.csv("AllStationsFormatted.csv")
 
 # one big happy family
 bigDF <- rbind(peccary_agouti, paca_agouti, ocelot_agouti)
@@ -29,14 +30,56 @@ communities <- factor(unique(bigDF$Community),
 )
 species <- unique(bigDF$Species1)
 
+# N for each community (AKA the number of sites at each community)
+nSites <- data.frame(
+    Community = communities,
+    nSites = c(
+        length(unique(Traps$Station[Traps$Community == "Zabalo"])),
+        length(unique(Traps$Station[Traps$Community == "Remolino"])),
+        length(unique(Traps$Station[Traps$Community == "Sinangoe"])),
+        length(unique(Traps$Station[Traps$Community == "San Pablo"])),
+        length(unique(Traps$Station[Traps$Community == "Siona"]))
+    )
+)
+
+# get Hedge's g for each community
+HedgesG <- bigDF[,c("Community", "Species1", "Species2")]
+HedgesG <- HedgesG[!duplicated(HedgesG), ]
+HedgesG$g <- NA
+HedgesG$SD1 <- NA
+HedgesG$SD2 <- NA
+HedgesG$SE1 <- NA
+HedgesG$SE2 <- NA
+
+# loop through communities and species
 for (i in 1:length(communities)){
     for (j in 1:length(species)){
         # subset data
         subDF <- bigDF[bigDF$Community == communities[i] & bigDF$Species1 == species[j], ]
+
+        # get N
+        N <- nSites$nSites[nSites$Community == communities[i]]
+
+        # calculate hedge's g
+        SD1 <- subDF$SE[1] * N
+        SD2 <- subDF$SE[2] * N
+        pooledSD <- sqrt(((N - 1) * (SD1^2) + (N - 1) * (SD2^2)) / (N + N - 2)) # https://www.statisticshowto.com/pooled-standard-deviation/
+        #pooledSD <- sqrt((SD1^2 + SD2^2) / 2) # https://www.statisticshowto.com/pooled-standard-deviation/
+        mean1 <- subDF$Occupancy[1]
+        mean2 <- subDF$Occupancy[2]
+        g <- (mean1 - mean2) / pooledSD
+
+        # store results
+        HedgesG$g[HedgesG$Community == communities[i] & HedgesG$Species1 == species[j]] <- g
+        HedgesG$SD1[HedgesG$Community == communities[i] & HedgesG$Species1 == species[j]] <- SD1
+        HedgesG$SD2[HedgesG$Community == communities[i] & HedgesG$Species1 == species[j]] <- SD2
+        HedgesG$SE1[HedgesG$Community == communities[i] & HedgesG$Species1 == species[j]] <- subDF$SE[1]
+        HedgesG$SE2[HedgesG$Community == communities[i] & HedgesG$Species1 == species[j]] <- subDF$SE[2]
+        
         
     }
 
 }
 
-write.csv(peccary_agouti, "peccary_agouti.csv")
+write.csv(changethis, "changethis.csv")
 
